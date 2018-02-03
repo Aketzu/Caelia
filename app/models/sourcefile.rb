d19b08265ac9ed338abe96ff68c1e738b4c65cd4
Anssi Kolehmainen <anssi.kolehmainen@vilant.com>
2018-02-03 13:44:14 +0200

    Use VAAPI for preview encoding

--- app/models/sourcefile.rb
+++ app/models/sourcefile.rb
@@ -19,9 +19,10 @@ class Sourcefile < ActiveRecord::Base
 
   def video_path_mp4
     fn = recording.name + "/" + File.basename(filename, ".nut") + ".mp4"
-    #Create MP4 version for Chrome
+    # Create MP4 version for Chrome
     unless File.exists?(recording.basepath + "/" + fn)
-      system("ffmpeg -v 0 -i \"%s\" -c:v copy -c:a copy \"%s\"" % [recording.basepath+"/"+video_path, recording.basepath+"/"+fn])
+      # system("ffmpeg -v 0 -i \"%s\" -c:v copy -c:a copy \"%s\"" % [recording.basepath+"/"+video_path, recording.basepath+"/"+fn])
+      system("ffmpeg -init_hw_device vaapi=foo:/dev/dri/renderD128 -hwaccel vaapi -hwaccel_output_format vaapi -hwaccel_device foo -i \"%s\" -filter_hw_device foo -vf 'hwupload,fps=10,scale_vaapi=w=1280:h=-2:format=nv12' -c:v h264_vaapi -level 40 -b:v 2M -maxrate 2M -c:a aac -y \"%s\"" % [recording.basepath+"/"+video_path, recording.basepath+"/"+fn])
       system("mp4file --optimize \"%s\"" % [recording.basepath+"/"+fn])
     end
 
