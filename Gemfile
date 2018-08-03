cea3a020a33490b4322fe0a7d56e82388013eac8
Anssi Kolehmainen <anssi@aketzu.net>
2018-08-03 16:02:11 +0300

    Update to Rails 5.2

--- Gemfile
+++ Gemfile
@@ -2,9 +2,9 @@ source 'https://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.10'
+gem 'rails', '~> 5.2.0'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+#gem 'sqlite3'
 gem 'pg', '~> 0.21'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
@@ -12,18 +12,18 @@ gem 'bootstrap-sass', '~> 3.3.6'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.1.0'
+gem 'coffee-rails', '~> 4.2'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.0'
+gem 'jbuilder', '~> 2.5'
 # bundle exec rake doc:rails generates the API under doc/api.
-gem 'sdoc', '~> 0.4.0', group: :doc
+#gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
@@ -33,20 +33,25 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'bootsnap', '>= 1.1.0', require: false
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
+  gem 'web-console', '~> 3.3.0'
+  gem 'listen', '>= 3.0.5', '< 3.2'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
 gem 'puma'
 gem 'rufus-scheduler'
+gem 'que'
 gem 'streamio-ffmpeg'
 gem 'therubyracer'
 gem 'yt'
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,43 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150731122428) do
+ActiveRecord::Schema.define(version: 2015_07_31_122428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "recordings", force: :cascade do |t|
-    t.string   "basepath"
-    t.string   "name"
+  create_table "que_jobs", primary_key: ["queue", "priority", "run_at", "job_id"], comment: "3", force: :cascade do |t|
+    t.integer "priority", limit: 2, default: 100, null: false
+    t.datetime "run_at", default: -> { "now()" }, null: false
+    t.bigserial "job_id", null: false
+    t.text "job_class", null: false
+    t.json "args", default: [], null: false
+    t.integer "error_count", default: 0, null: false
+    t.text "last_error"
+    t.text "queue", default: "", null: false
+  end
+
+  create_table "recordings", id: :serial, force: :cascade do |t|
+    t.string "basepath"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "sourcefiles", force: :cascade do |t|
-    t.integer  "recording_id"
-    t.integer  "nr"
-    t.string   "filename"
-    t.decimal  "length",       precision: 10, scale: 2
+  create_table "sourcefiles", id: :serial, force: :cascade do |t|
+    t.integer "recording_id"
+    t.integer "nr"
+    t.string "filename"
+    t.decimal "length", precision: 10, scale: 2
     t.datetime "recorded_at"
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recording_id"], name: "index_sourcefiles_on_recording_id"
   end
 
-  add_index "sourcefiles", ["recording_id"], name: "index_sourcefiles_on_recording_id", using: :btree
-
-  create_table "vods", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "recording_id"
-    t.decimal  "start_pos",    precision: 10, scale: 2
-    t.decimal  "end_pos",      precision: 10, scale: 2
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
-    t.float    "encode_pos"
-    t.integer  "status"
-    t.integer  "elaineid"
+  create_table "vods", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.integer "recording_id"
+    t.decimal "start_pos", precision: 10, scale: 2
+    t.decimal "end_pos", precision: 10, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "encode_pos"
+    t.integer "status"
+    t.integer "elaineid"
+    t.index ["recording_id"], name: "index_vods_on_recording_id"
   end
-
-  add_index "vods", ["recording_id"], name: "index_vods_on_recording_id", using: :btree
 
   add_foreign_key "sourcefiles", "recordings"
   add_foreign_key "vods", "recordings"
